ed2d73542ba13b9876ffa8c530ba1a2174d4438e
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2011-02-04 01:58:21 +0300

    Test code: simple bean

---
+++ src/java/sandbox/dan/vjtest/sandbox/annotations/temp/Bean.java
@@ -0,0 +1,16 @@
+package dan.vjtest.sandbox.annotations.temp;
+
+/**
+ * @author Alexander Dovzhikov
+ */
+public class Bean {
+    private int f;
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+}
