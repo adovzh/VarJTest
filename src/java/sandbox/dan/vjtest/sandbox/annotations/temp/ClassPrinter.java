4ca0ae6543795f3e1da32b7f9ef683d40bea4a2d
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2011-02-04 00:17:52 +0300

    Some testing code: simple class printer

---
+++ src/java/sandbox/dan/vjtest/sandbox/annotations/temp/ClassPrinter.java
@@ -0,0 +1,53 @@
+package dan.vjtest.sandbox.annotations.temp;
+
+import org.objectweb.asm.*;
+
+import java.io.IOException;
+
+/**
+ * @author Alexander Dovzhikov
+ */
+public class ClassPrinter implements ClassVisitor {
+
+    private final int debug = 1;
+
+    public static void main(String[] args) throws IOException {
+        ClassPrinter cp = new ClassPrinter();
+        ClassReader cr = new ClassReader("dan.vjtest.sandbox.annotations.temp.ClassPrinter");
+        cr.accept(cp, 0);
+    }
+
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        System.out.println(name + " extends " + superName + " {");
+    }
+
+    public void visitSource(String source, String debug) {
+    }
+
+    public void visitOuterClass(String owner, String name, String desc) {
+    }
+
+    public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+        return null;
+    }
+
+    public void visitAttribute(Attribute attr) {
+    }
+
+    public void visitInnerClass(String name, String outerName, String innerName, int access) {
+    }
+
+    public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+        System.out.println("   " + desc + " " + name);
+        return null;
+    }
+
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        System.out.println("   " + name + ":" + desc);
+        return null;
+    }
+
+    public void visitEnd() {
+        System.out.println("}");
+    }
+}
