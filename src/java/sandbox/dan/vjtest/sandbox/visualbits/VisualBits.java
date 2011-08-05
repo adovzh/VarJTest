688028a260c16a883096783632792938fc41e865
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2011-08-05 21:21:11 +0400

    Another buggy implementation

--- src/java/sandbox/dan/vjtest/sandbox/visualbits/VisualBits.java
+++ src/java/sandbox/dan/vjtest/sandbox/visualbits/VisualBits.java
@@ -22,7 +22,22 @@ public class VisualBits extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        String binaryString = Integer.toBinaryString(model.getValue());
+        String binaryString = toBinaryString(model.getValue());
         g.drawString(binaryString, 10, 10);
+    }
+
+    private static String toBinaryString(int value) {
+        StringBuilder sb = new StringBuilder(40);
+        int i;
+
+        for (i = 31; i >= 0; i--) {
+            sb.append(((value & (1 << i)) > 0) ? '1' : '0');
+
+            if (i != 0 && (i & 3) == 0) {
+                sb.append(' ');
+            }
+        }
+
+        return sb.toString();
     }
 }
