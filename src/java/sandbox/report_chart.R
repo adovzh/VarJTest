caf3ba00d689f012e9a924408540f9e7f6ca730e
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2012-02-12 01:38:10 +0400

    Sandbox: R script parametrised

--- src/java/sandbox/report_chart.R
+++ src/java/sandbox/report_chart.R
@@ -1,3 +1,7 @@
+# parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+report_var <- if (length(args) > 0) args[1] else "Latency"
+
 # read data
 report <- read.csv("test_locks_report.csv")
 
@@ -5,20 +9,24 @@ report <- read.csv("test_locks_report.csv")
 specs <- levels(report$Spec)
 nspecs <- length(specs)
 xrange <- range(report$Number)
-yrange <- range(report$Duration)
+yrange <- range(report[[report_var, exact=FALSE]])
 
 # set up the plot
-plot(xrange, yrange, type="n", xlab="Number of threads", ylab="Duration")
+pdf(paste("chart_", tolower(report_var), ".pdf", sep=""))
+plot(xrange, yrange, type="n", xlab="Number of threads", ylab=report_var)
 colors <- rainbow(nspecs)
 i <- 1
 
 # add lines
 for (spec in specs) {
 	specset <- subset(report, Spec == spec)
-	lines(specset$Number, specset$Duration, type="b", col=colors[i])
+	lines(specset$Number, specset[[report_var, exact=FALSE]], type="b", col=colors[i])
 	i <- i + 1
 }
 
 # add title
 title("Fucking chart I am pissed of")
+
+# good bye
+dev.off()
 
