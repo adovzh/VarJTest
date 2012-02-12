9d1ee53f8b9280457f03c8f6e79c158caf4e1f3e
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2012-02-13 02:33:13 +0400

    Sandbox: R script custom y axis

--- src/java/sandbox/report_chart.R
+++ src/java/sandbox/report_chart.R
@@ -12,15 +12,18 @@ cur_index <- indices[[report_var]]
 report <- read.csv("test_locks_report.csv", check.names=FALSE)
 
 # prepare additional variables
-specs <- levels(report$Spec)
+# specs <- levels(report$Spec)
+specs <- c("JVM", "JVM_Biased", "JUC")
 nspecs <- length(specs)
 xrange <- range(report$Number)
 yrange <- range(report[[report_var, exact=FALSE]])
 ylabs <- names(report)[indices + 2]
+yticks <- seq(0, max(report[[report_var, exact=FALSE]], by=5e+7))
 
 # set up the plot
 pdf(paste("chart_", tolower(report_var), ".pdf", sep=""))
-plot(xrange, yrange, type="n", xlab="Number of threads", ylab=ylabs[cur_index])
+plot(xrange, yrange, type="n", xlab="Number of threads", ylab=ylabs[cur_index], yaxt="n")
+axis(2, las=2)
 colors <- rainbow(nspecs)
 i <- 1
 
@@ -31,8 +34,9 @@ for (spec in specs) {
 	i <- i + 1
 }
 
-# add title
+# add title and legend
 title("Fucking chart I am pissed of")
+legend("topright", inset=.05, c("-UseBiasedLocking", "+UseBiasedLocking", "ReentrantLock"), fill=colors)
 
 # good bye
 dev.off()
